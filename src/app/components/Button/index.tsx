d109896344056d94b12f384a499a3194dbded683
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-01-21 16:56:54 -0300

    feat: create component Button

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles"
+
+type ButtonProps = {
+  children: React.ReactNode
+  variant?: 'primary' | 'secondary' | 'alternate'
+  disabled?: boolean
+  onClick?: () => void
+}
+
+export function Button({ children, variant = 'primary', disabled = false, onClick }: ButtonProps) {
+  return (
+    <Container variant={variant} disabled={disabled} onClick={onClick}>
+      {children}
+    </Container>
+  )
+}
---
+++ src/app/components/Button/styles.ts
@@ -0,0 +1,58 @@
+import styled, { css } from 'styled-components'
+
+type ButtonProps = {
+  variant?: 'primary' | 'secondary' | 'alternate'
+  disabled?: boolean
+}
+
+export const Container = styled.button<ButtonProps>`
+  padding: 10px 32px;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  ${({ variant, disabled }) => {
+    switch (variant) {
+      case 'primary':
+        return css`
+          background-color: ${disabled ? '#b3d4fc' : '#0056D2'};
+          color: ${disabled ? '#ffffff' : '#ffffff'};
+          &:hover {
+            background-color: ${disabled ? '#b3d4fc' : '#2342C0'};
+          }
+        `
+      case 'secondary':
+        return css`
+          background-color: ${disabled ? '#d3c6f3' : '#7048E8'};
+          color: ${disabled ? '#ffffff' : '#ffffff'};
+          &:hover {
+            background-color: ${disabled ? '#d3c6f3' : '#5028C6'};
+          }
+        `
+      case 'alternate':
+        return css`
+          background-color: ${disabled ? '#f4f4f4' : '#ffffff'};
+          border: 2px solid ${disabled ? '#f4f4f4' : '#ACB5BD'};
+          color: ${disabled ? '#000000' : '#495057'};
+          &:hover {
+            background-color: ${disabled ? '#f4f4f4' : '#e6e6e6'};
+            border: 2px solid ${disabled ? '#f4f4f4' : '#000000'};
+          }
+        `
+      default:
+        return css`
+          background-color: #f4f4f4;
+          color: #000000;
+        `
+    }
+  }}
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      cursor: not-allowed;
+    `}
+`
