70a194c3de6e72d6cd40ad53ab4a2e9d70a8f0bc
Janderson Vilas Boas <jandersonvb.dev@gmail.com>
2025-01-21 22:48:32 -0300

    feat: create component DateFilter

---
+++ src/app/components/DateFilter/index.tsx
@@ -0,0 +1,30 @@
+import { Button } from "../Button";
+import { Container, WrapperEndDate, WrapperStartDate } from "./styles";
+
+export function DateFilter() {
+  return (
+    <Container>
+      <WrapperStartDate>
+        <label htmlFor="startDate" className="block text-gray-500">Início</label>
+        <input
+          type="date"
+          id="startDate"
+          className="border p-2 rounded"
+        />
+      </WrapperStartDate>
+      <WrapperEndDate>
+        <label htmlFor="endDate" className="block text-gray-500">Fim</label>
+        <input
+          type="date"
+          id="endDate"
+          className="border p-2 rounded"
+        />
+      </WrapperEndDate>
+      <Button
+        variant="primary"
+      >
+        Filtrar por data
+      </Button>
+    </Container>
+  )
+} 
---
+++ src/app/components/DateFilter/styles.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+
+  gap: 66px;
+
+
+  //centralizar o container
+  justify-content: center;
+  margin-top: 100px;
+
+
+`
+export const WrapperStartDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
+
+export const WrapperEndDate = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
